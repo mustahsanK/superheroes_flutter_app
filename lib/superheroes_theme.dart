c53e422897f2bf288a5f6faba08b69e7528d22d6
mustahsanK <mustahsan60@outlook.com>
2023-07-17 23:41:33 +0530

    theme changes

--- lib/superheroes_theme.dart
+++ lib/superheroes_theme.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 ThemeData appTheme = ThemeData(
-  brightness: Brightness.dark,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark,
+    primary: Color(0xff121012),
+    onPrimary: Color(0xffffffff),
+    secondary: Color(0xff382C3E),
+    onSecondary: Color(0xffffffff),
+    error: Colors.redAccent,
+    onError: Color(0xffffffff),
+    background: Colors.black45,
+    onBackground: Color(0xffffffff),
+    surface: Colors.black45,
+    onSurface: Color(0xffffffff),
+  ),
   fontFamily: 'Kanit'
 );
