378609578865825141b5db4938750a2bb6a2ee40
mustahsanK <mustahsan60@outlook.com>
2023-08-29 19:40:24 +0530

    get hero service

---
+++ lib/services/get_hero.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../access_token.dart';
+import '../models/superhero.dart';
+
+Future<Superhero> getHero(String id) async {
+  Uri url = Uri.parse('https://superheroapi.com/api/$accessToken/search/$id');
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return Superhero.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Error');
+  }
+}
