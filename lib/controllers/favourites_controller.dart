113a9bd432fb529410c0caad6566947835c1ac7a
mustahsanK <mustahsan60@outlook.com>
2023-08-29 19:40:56 +0530

    updated controller to work with database and api

--- lib/controllers/favourites_controller.dart
+++ lib/controllers/favourites_controller.dart
@@ -1,10 +1,26 @@
 import 'package:get/get.dart';
+
+import 'package:superheroes_app/database/superheroes_database.dart';
 import 'package:superheroes_app/models/superhero.dart';
+import 'package:superheroes_app/services/get_hero.dart';
 
 class FavouritesController extends GetxController {
-  List<Superhero> favouriteHeroes = List<Superhero>.empty().obs;
+  List<Superhero> favouriteHeroes = List<Superhero>.empty(growable: true).obs;
 
-  addToFavourites(Superhero hero) {
+  getAllFavourites() async {
+    final List<Map<String, dynamic>> maps = await SuperheroesDatabase().getAllRows();
+    for (var map in maps) {
+      favouriteHeroes.add(await getHero(map['id']));
+    }
+  }
+
+  addToFavourites(Superhero hero) async {
+    await SuperheroesDatabase().insertIntoTable(hero);
     favouriteHeroes.add(hero);
+  }
+
+  deleteFromFavourites(Superhero hero) async {
+    await SuperheroesDatabase().deleteFromTable(hero);
+    favouriteHeroes.removeWhere((element) => element.id == hero.id);
   }
 }
