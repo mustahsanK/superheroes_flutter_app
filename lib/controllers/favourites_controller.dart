a23e109fef95f361c2ab52277992d47f890e493c
mustahsanK <mustahsan60@outlook.com>
2023-08-29 23:02:20 +0530

    set data structure to contain ids of favourite heroes

--- lib/controllers/favourites_controller.dart
+++ lib/controllers/favourites_controller.dart
@@ -6,21 +6,25 @@ import 'package:superheroes_app/services/get_hero.dart';
 
 class FavouritesController extends GetxController {
   List<Superhero> favouriteHeroes = List<Superhero>.empty(growable: true).obs;
+  Set<String> idSet = {};
 
   getAllFavourites() async {
     final List<Map<String, dynamic>> maps = await SuperheroesDatabase().getAllRows();
     for (var map in maps) {
       favouriteHeroes.add(await getHero(map['id']));
+      idSet.add(map['id']);
     }
   }
 
   addToFavourites(Superhero hero) async {
     await SuperheroesDatabase().insertIntoTable(hero);
     favouriteHeroes.add(hero);
+    idSet.add(hero.id);
   }
 
   deleteFromFavourites(Superhero hero) async {
     await SuperheroesDatabase().deleteFromTable(hero);
     favouriteHeroes.removeWhere((element) => element.id == hero.id);
+    idSet.remove(hero.id);
   }
 }
